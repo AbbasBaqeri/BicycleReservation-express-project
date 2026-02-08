461ee6ccd1f53d8d91b8ab9c11bd7c61acaac0c3
Abbas Baqeri <liamcox1888@gmail.com>
2026-02-08 22:52:49 +0330

    Update store.js

--- data/store.js
+++ data/store.js
@@ -1,1 +1,14 @@
-aa
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, 'store.json')
+
+function readData() {
+  return JSON.parse(fs.readFileSync(filePath))
+}
+
+function writeData(data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2))
+}
+
+module.exports = { readData, writeData }
